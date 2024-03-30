40fb6569f50f8a5fa5205af6abc994974afed100
villaclara <villaclara1231@gmail.com>
2024-03-30 03:27:25 +0200

    design trackeditemapp template

---
+++ src/Desktop/UI.WPF/Components/TrackedAppItem.xaml.cs
@@ -0,0 +1,47 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI.WPF.Components;
+
+/// <summary>
+/// Interaction logic for TrackedAppItem.xaml
+/// </summary>
+public partial class TrackedAppItem : UserControl
+{
+	public TrackedAppItem()
+	{
+		InitializeComponent();
+		SeriesCollection = new SeriesCollection()
+		{
+			new ColumnSeries
+			{
+				Title = "Time",
+				Values = new ChartValues<double> { 10, 20, 30, 0, 20, 1, 0 }
+			}
+		};
+
+
+		Labels = new[] { "23/03", "24/03", "25/03", "26/03", "27/03", "28/03", "29/03" };
+		Formatter = value => value.ToString("N");
+
+		DataContext = this;
+	}
+
+	public SeriesCollection SeriesCollection { get; set; }
+	public string[] Labels { get; set; }
+	public Func<double, string> Formatter { get; set; }
+}
