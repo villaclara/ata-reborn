1973f48525ae12e4953f38317a6de0cc654ae6e3
villaclara <villaclara1231@gmail.com>
2024-04-10 22:29:25 +0300

    wip/ add process to tracking

--- src/Desktop/UI.WPF/ViewModels/ProcessListViewModel.cs
+++ src/Desktop/UI.WPF/ViewModels/ProcessListViewModel.cs
@@ -1,35 +1,62 @@
 ﻿using Application.Common.Abstracts;
 using Application.Director.Instance;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Serilog;
 using Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UI.WPF.Services;
 
 namespace UI.WPF.ViewModels;
 
 public partial class ProcessListViewModel : BaseViewModel
 {
-	private readonly IDirector _director;
-	private readonly IGetProcs _getProcessesService;
-
-	public ProcessListViewModel(IDirector director, IGetProcs getProcessesService)
+	public ProcessListViewModel(IDirector director, IGetProcs getProcessesService, INavigationService navigation)
 	{
-		this._director = director;
+		_director = director;
+		_navigation = navigation;
 		_getProcessesService = getProcessesService;
 		ProcessesList = _getProcessesService.GetUniqueProcessesAsList(useSystemManagement: false).OrderBy(p => p.ProcessName);
 	}
 
+
+	private readonly IDirector _director;
+	private readonly IGetProcs _getProcessesService;
+	private readonly INavigationService _navigation;
+
 	public IEnumerable<UniqueProcess> ProcessesList { get; }
 
-
+	[ObservableProperty]
+	public UniqueProcess? _selectedProcess;
 
 	[RelayCommand]
-	public void AddSelectedAppToTrack()
+	public void AddSelectedAppToTrack(UniqueProcess selectedProcess)
 	{
-		_director.AddAppToTrackedList("");
+		try
+		{
+			Log.Information("{@Method} - Try block for adding ({@app}) by director.", nameof(AddSelectedAppToTrack), selectedProcess?.ProcessName);
+			_director.AddAppToTrackedList(selectedProcess!.ProcessName, selectedProcess.AppName ?? null);
+		}
+		catch(Exception ex)
+		{
+			Log.Error("{@Method} - Exception ({@ex}) when adding ({@app}). App was not added.", nameof(AddSelectedAppToTrack), ex.Message, selectedProcess?.ProcessName);
+		}
+		finally
+		{
+			_navigation.NavigateTo<TrackedAppsViewModel>();
+			this.Dispose();
+		}
+
 	}
 
+	public void Dispose()
+	{
+
+	}
 }
+
+
