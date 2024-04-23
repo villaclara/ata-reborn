6935f647d30711e2767c69415f47fcf08c022ec3
villaclara <villaclara1231@gmail.com>
2024-04-23 19:31:26 +0300

    save files inside AppData/Roaming dir

--- src/Application/Utilities/ConstantValues.cs
+++ src/Application/Utilities/ConstantValues.cs
@@ -8,11 +8,51 @@ namespace Application.Utilities;
 
 public static class ConstantValues
 {
-	public static string MAIN_FILE_NAME = "apps.json";
-	public static string BACKUP_MAIN_FILE_NAME = "apps_backup.json";
+
+	// path to AppData/Roaming/
+	private static readonly string _appDataRoamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+	// Full path to AppData/Roaming/ATA Reborn. 
+	// Is used when creating MAIN_FILE and LOG_FILE.
+	public static string DIRECTORY_FILE_NAME { get; }
+
+	private static string _main_filename = "apps.json";
+	public static string MAIN_FILE_NAME 
+	{ 
+		get => _main_filename; 
+		set => _main_filename = Path.Combine(DIRECTORY_FILE_NAME, value); 
+	}
+
+
+	private static string _backup_main_filename = "apps_backup.json";
+	public static string BACKUP_MAIN_FILE_NAME
+	{
+		get => _backup_main_filename;
+		set => _backup_main_filename = Path.Combine(DIRECTORY_FILE_NAME, value);
+	}
+
+	private static readonly string _log_filename;
+	public static string LOG_FILE_NAME
+	{
+		get => _log_filename;
+	}
 
 
 	// THIS TWO SHOULD BE CHANGED TOGETHER. M - Minutes, MS - Miliseconds. 
 	public static int TIMER_INTERVAL_M = 1;
 	public static int TIMER_INTERVAL_MS = 15000;
+
+	static ConstantValues()
+	{
+		// create if needed directory AppData/Roaming/ATA Reborn
+		var dirPath = Path.Combine(_appDataRoamingPath, "ATA Reborn");
+		if(!Directory.Exists(dirPath))
+		{
+			Directory.CreateDirectory(dirPath);
+		}
+
+		DIRECTORY_FILE_NAME = dirPath;
+
+		_log_filename = Path.Combine(DIRECTORY_FILE_NAME, "log.txt");
+	}
 }
