81f47aef98fd7a09d5afd0bbba10659e8e7d76d7
villaclara <villaclara1231@gmail.com>
2024-04-29 14:53:43 +0300

    regedit autolaunch on win start

---
+++ src/Desktop/UI.WPF/Utilities/RegistryEditor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.WPF.Utilities;
+
+public static class RegistryEditor
+{
+	public static int SetAppToLaunchOnStartup()
+	{
+		RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true)!;
+		reg.SetValue("ATAReborn", Process.GetCurrentProcess().MainModule!.FileName.ToString());
+		Log.Information("{@Method} - Set value({@val}) in registry.", nameof(SetAppToLaunchOnStartup), "ATAReborn");
+		return 0;
+	}
+
+	public static int RemoveAppFromLaunchOnStartup()
+	{
+		RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true)!;
+		reg.DeleteValue("ATAReborn");
+		Log.Information("{@Method} - Del value({@val}) from registry.", nameof(RemoveAppFromLaunchOnStartup), "ATAReborn");
+		return 0;
+	}
+}
