3b2c51c6200581ec247921c5553cc44c9d6150b7
villaclara <villaclara1231@gmail.com>
2025-02-28 00:44:22 +0200

    scrollbar for changelog page

--- src/Desktop/UI.WPF/ViewModels/ChangelogPageViewModel.cs
+++ src/Desktop/UI.WPF/ViewModels/ChangelogPageViewModel.cs
@@ -1,19 +1,24 @@
-﻿using UI.WPF.Services.Abstracts;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using UI.WPF.Services.Abstracts;
 
 namespace UI.WPF.ViewModels;
 
-public class ChangelogPageViewModel : BaseViewModel
+public partial class ChangelogPageViewModel : BaseViewModel
 {
 	private readonly IChangelogService _changelogService;
 
-	public IEnumerable<SingleVersionChangelogNote> ChangelogNotes { get; } = [];
+
+	[ObservableProperty]
+	private IEnumerable<SingleVersionChangelogNote> _changelogNotes = [];
+
+	//public IEnumerable<SingleVersionChangelogNote> ChangelogNotes { get; } = [];
 
 	public ChangelogPageViewModel(IChangelogService changelogService)
 	{
 
 		_changelogService = changelogService;
 
-		ChangelogNotes = _changelogService.GetAllChangelog();
+		ChangelogNotes = _changelogService.GetAllChangelog().Reverse();
 	}
 
 	public string TextName { get; set; } = "BRUH";
